0582d2c6947d953b6c3d9aedf6f0d0c60ac9fa43
jtan <janjtan@icloud.com>
2018-04-12 14:11:26 +0800

    index:添加了监控停启功能;log停启\清空功能;添加了状态刷新频率的选择; socketio:配合上方功能,新增了监听disconnect和freq事件;

--- app/socketios.py
+++ app/socketios.py
@@ -1,12 +1,13 @@
 # coding: utf8
 from app import app
 from threading import Lock
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, disconnect
 import psutil, time
 
 # 监控线程
 thread = None
 thread_lock = Lock()
+freq = 5
 
 async_mode = None
 socketio = SocketIO(app, async_mode=async_mode)
@@ -15,29 +16,42 @@ socketio = SocketIO(app, async_mode=async_mode)
 # 后台线程 产生数据，即刻推送至前端
 def background_thread():
     """Example of how to send server generated events to clients."""
-    # count = 0
+    global freq
+    count = 0
     netlist = [psutil.net_io_counters().bytes_recv]
     while True:
-        socketio.sleep(2)
-        # count += 1
-        # t = time.strftime('%H:%M:%S', time.localtime())  # 获取系统时间
+        socketio.sleep(freq)
+        count += 1
+        t = time.strftime('%H:%M:%S', time.localtime())  # 获取系统时间
         cpus = round(sum(psutil.cpu_percent(interval=None, percpu=True)) / 4, 2)  # 获取系统cpu使用率 non-blocking
         mem = psutil.virtual_memory().percent
         netlist.append(psutil.net_io_counters().bytes_recv)
         net = round((netlist[-1] - netlist[-2]) / ((1024 ** 2) * 3) * 100, 2)
         netlist.pop(0)
 
-        data = {"data": [cpus, mem, net]}
+        data = {"data": [cpus, mem, net, count, t]}
         socketio.emit('server_response', data, namespace='/serverow')  # 注意：这里不需要客户端连接的上下文，默认 broadcast = True ！
 
 
 # 与前端建立 socket 连接后，启动后台线程
 @socketio.on('connect', namespace='/serverow')
-def test_connect():
+def stat_connect():
     global thread
+    print('client-side connected')
     with thread_lock:
         if thread is None:
-            thread = socketio.start_background_task(target=background_thread)
+            thread = socketio.start_background_task(background_thread)
+
+@socketio.on('disconnect', namespace='/serverow')
+def stat_end():
+    disconnect(namespace='/serverow')
+    print('client-side disconnect')
+
+@socketio.on('freq', namespace='/serverow')
+def change_freq(myfreq):
+    global freq
+    freq = int(myfreq['data'])
+    print("change freq to %d second" % freq)
 
 
 @socketio.on('connect', namespace='/test')
@@ -45,14 +59,9 @@ def chat_connect():
     socketio.emit('server_sent', {'data': 'Connected!'}, namespace='/test')
 
 
-count = 0
-
-
 @socketio.on('client_sent', namespace='/test')
 def chat_loop(msg):
-    global count
-    count += 1
     if msg:
         socketio.sleep(5)
-        socketio.emit('server_sent', {'data': str(count)}, namespace='/test')
+        socketio.emit('server_sent', {'data': 'here'}, namespace = '/test')
         print(msg['data'])
